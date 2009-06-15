568aff668041acc8dfc005681026858ca19fe2cc
Christian Weis <christian.weis@gmail.com>
2009-06-16 03:15:11 +0800

    testing if .url produces correct urls
    Signed-off-by: Alan Peabody <alan@gapdesigns.com>

--- app/helpers/robot_helper.rb
+++ app/helpers/robot_helper.rb
@@ -3,11 +3,11 @@ module RobotHelper
   def build_full_url(page)
     hostname = Radiant::Config["rad_seo.domain"]
     temp = page
-    url = ""
-    while !temp.parent.nil?
-      url = temp.slug + '/' + url
-      temp = temp.parent
-    end
+    url = temp.url
+    # while !temp.parent.nil?
+    #   url = temp.slug + '/' + url
+    #   temp = temp.parent
+    # end
     return hostname + url
   end
 
